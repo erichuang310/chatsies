0688cace2867ff883959662eed6ea4cb43083544
Eric Huang <eshuang@ucdavis.edu>
2014-10-11 15:14:11 -0700

    ...

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express')
 var app = express();
 var port = process.env.PORT || 5000;
 
-  console.log("WTF");
 var io = require('socket.io').listen(app.listen(port));
 var chatServer = require('./chat_server').ChatServer(io);
 
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 module.exports = function(app,io){
 	app.get('/', function(req, res){
+		console.log("Got into the routes!");
 		res.render('index');
 	});
 };
