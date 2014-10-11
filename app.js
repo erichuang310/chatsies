df7c01c2867bb1c198d13247f688d60890154065
Eric Huang <eshuang@ucdavis.edu>
2014-10-11 14:19:00 -0700

    Still fixing heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var http = require('http');
 var nodeStatic = require('node-static');
 var socketio = require('socket.io');
 
+var port = process.env.PORT || 8080;
+
 var file = new nodeStatic.Server('./public');
 
 var server = http.createServer(function (req, res) {
@@ -14,4 +16,4 @@ var server = http.createServer(function (req, res) {
 
 var chatServer = new require("./chat_server").ChatServer(server);
 
-server.listen(8000);
+// server.listen(port);
