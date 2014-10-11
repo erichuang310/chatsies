9b5d17c115bc790cfb70f893b63c4910df9a7eee
Eric Huang <eshuang@ucdavis.edu>
2014-10-11 14:23:22 -0700

    ...

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var http = require('http');
 var nodeStatic = require('node-static');
 var socketio = require('socket.io');
 
-var port = process.env.PORT || 8080;
-
 var file = new nodeStatic.Server('./public');
+
+var port = process.env.PORT || 8080;
 
 var server = http.createServer(function (req, res) {
   req.addListener('end', function () {
@@ -16,4 +16,4 @@ var server = http.createServer(function (req, res) {
 
 var chatServer = new require("./chat_server").ChatServer(server);
 
-// server.listen(port);
+server.listen(port);
